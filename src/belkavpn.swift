f4d9df3fb915659b214b40241bc787eb7fec0d71
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-08-25 18:45:50 +0300

    Update belkavpn.swift

--- src/belkavpn.swift
+++ src/belkavpn.swift
@@ -20,6 +20,14 @@ extension URLSession {
     }
 }
 
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
+
 public class Belkavpn{
     private let api = "https://api.belkavpn.com"
     private var headers: [String: String]
